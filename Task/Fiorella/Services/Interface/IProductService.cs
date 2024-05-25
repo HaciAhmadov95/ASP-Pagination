8853af532b0e5f8a5cc668d97c4b15ef915a13e3
haci1995 <hajiaghaaa@code.edu.az>
2024-05-26 03:29:13 +0400

    first commit

---
+++ Task/Fiorella/Services/Interface/IProductService.cs
@@ -0,0 +1,15 @@
+﻿using Fiorella.Models;
+using Fiorella.ViewModels.Products;
+
+namespace Fiorella.Services.Interface
+{
+    public interface IProductService
+    {
+        Task<List<Product>> GetAllWithImagesAsync();
+        Task<Product> GetByIdAsync(int id);
+        Task<List<Product>> GetAllAsync();
+        List<ProductVM> GetMappedDatas(List<Product> products);
+        Task<List<Product>> GetAllPaginateAsync(int page, int take = 4);
+        Task<int> GetCountAsync();
+    }
+}
